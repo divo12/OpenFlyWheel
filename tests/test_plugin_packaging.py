b434ac9f49f386940bf1c5a67a6b1a8c35948963
divo12 <divyansh_b@ch.iitr.ac.in>
2026-08-28 10:59:50 +0530

    chore: pin simplified failure workspace runtime

--- tests/test_plugin_packaging.py
+++ tests/test_plugin_packaging.py
@@ -28,7 +28,7 @@ def test_openflywheel_mcp_uses_pinned_portable_runtime() -> None:
     server = manifest.mcpServers["openflywheel"]
 
     assert (
-        "git+https://github.com/divo12/OpenFlyWheel.git@d663480401ab590288781d02d882cda86740941e"
+        "git+https://github.com/divo12/OpenFlyWheel.git@fed98e05ea2890d671baab54e9bba7a8dbb28cc0"
         in server.args
     )
     assert "openflywheel-mcp" in server.args
