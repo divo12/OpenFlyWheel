f3f3cfeccd713c75c53c468e6f7a58181c54429d
divo12 <divyansh_b@ch.iitr.ac.in>
2026-09-02 15:38:05 +0530

    chore: pin opaque identifier fix

--- tests/test_plugin_packaging.py
+++ tests/test_plugin_packaging.py
@@ -28,7 +28,7 @@ def test_openflywheel_mcp_uses_pinned_portable_runtime() -> None:
     server = manifest.mcpServers["openflywheel"]
 
     assert (
-        "git+https://github.com/divo12/OpenFlyWheel.git@71ce85bfc43f8c7c42d96cce02b8351937515dcc"
+        "git+https://github.com/divo12/OpenFlyWheel.git@ab0ef62cbe1e6cddf0bfd8ec61374d10120c61aa"
         in server.args
     )
     assert "openflywheel-mcp" in server.args
